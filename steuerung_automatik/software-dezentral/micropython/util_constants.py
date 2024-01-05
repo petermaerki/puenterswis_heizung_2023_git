596d34615ef0fa8152774f083be8caf701d95de1
Hans Maerki <buhtig.hans.maerki@ergoinfo.ch>
2024-01-05 11:19:56 +0100

    New: controller_pymodbus

--- steuerung_automatik/software-dezentral/micropython/util_constants.py
+++ steuerung_automatik/software-dezentral/micropython/util_constants.py
@@ -2,6 +2,7 @@ def to_version(version):
     assert len(version) == 3
     return 10_000 * version[0] + 100 * version[1] + version[2]
 
+
 VERSION_HW = to_version((1, 0, 0))
 VERSION_SW = to_version((0, 1, 0))
 
